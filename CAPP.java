869c00cd5220c31171130a3db39dfc97ae6fb17f
RahulPardeshi <rahulpardeshi949@gmail.com>
2025-12-09 11:46:23 +0530

    this is ch to string application

---
+++ CAPP.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CAPP
+{
+   public static void main(String args[])
+   {
+      char ch[]=new char[]{'a','g','o','o','d','f','g','h','i','j'};
+     
+      String s=new String(ch,1,4);
+      System.out.println(s);
+   }
+}
