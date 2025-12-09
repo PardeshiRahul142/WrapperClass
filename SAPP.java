c6e1cd6a9fe4777f05d10eafd2dcc8bac5602cf8
RahulPardeshi <rahulpardeshi949@gmail.com>
2025-12-09 11:40:11 +0530

    this is string application

---
+++ SAPP.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SAPP
+{ 
+  public static void main(String args[])
+  {
+    String[] str={"Rahul","Rohan","Nilesh","Nikhil"};
+    for(String s:str)
+    {
+       System.out.println(s);
+    }
+  }
+}
